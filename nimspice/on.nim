3f2541aae6df4a297f3f53b363f0d1e6321d0089
CodeDoes <codedoes@gmail.com>
2018-07-15 03:58:12 +0200

    changed on macro, moved with macro

--- nimspice/on.nim
+++ nimspice/on.nim
@@ -1,26 +1,28 @@
 ## https://forum.nim-lang.org/t/4044#25189
 
-template `on`(xpr: typed, cond: untyped, onvalid: untyped): untyped =
+template `on`*(xpr: typed, cond: untyped, onvalid: untyped): untyped =
   var value = xpr
   block:
     template _ : untyped {.inject.} = value
     if cond:
       onvalid
   value
-template onError(xpr: typed, errBody: untyped): untyped =
+  # mixin a_val
+  # template b: untyped {.inject.} = a_val
+  # b
+template onError*(xpr: typed, errBody: untyped): untyped =
   xpr.on _ != 0:
     errBody
-var 
-  val = 
-    1.on _ > 0:
-      echo "valid value"
-      _ = 22222
-
-doAssert:
-  val == 22222
-doAssert:
-  (;1 onError:
-    echo "caught the error"
-    ) == 1
-
-
+    
+when isMainModule:
+  var 
+    val = 
+      1.on _ > 0:
+        echo "valid value"
+        _ = 22222
+  doAssert:
+    val == 22222
+  doAssert:
+    (;1.onError:
+      echo "caught the error"
+      ) == 1
